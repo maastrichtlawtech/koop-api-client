3816c0554b10b4443c7958ba884cd8002fd184ac
Carlos <carloseam94@gmail.com>
2022-12-19 17:56:33 +0100

    testing different encoding for matches in backend

--- api/find_citations.py
+++ api/find_citations.py
@@ -8,7 +8,7 @@ def query_api(xml_url, depth):
     response = requests.get(xml_url)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
-    citations = re.findall("<extref(.*?)</extref>", response.text)
+    citations = re.findall("<extref.*?<\/extref>", response.text)
     return citations
 
 
@@ -26,5 +26,5 @@ class handler(BaseHTTPRequestHandler):
         s = self.path
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         citations = query_api(dic["xml_url"], dic["depth"])
-        self.wfile.write(citations.encode('utf-8'))
+        self.wfile.write(str(citations))
         return
