dfc7e32f0226403faa92589e3c899eefd587cafd
Carlos <carloseam94@gmail.com>
2022-12-20 16:08:35 +0100

    show graph of citations

---
+++ pages/graph/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="my-3 text-center">Graph</h1>
+          <Graph :graph="graph"></Graph>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+    computed: {
+        graph() {
+            return this.$store.state.graph.graph;
+        }
+    },
+    mounted() {
+        console.log(this.graph);
+    },
+}
+</script>
